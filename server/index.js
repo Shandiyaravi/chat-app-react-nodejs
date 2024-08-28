2650bb1b892e22a2d7cb940b9e3a5255052ee095
shandiya ravi <shandiyaravi@gmail.com>
2024-08-28 12:46:28 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -7,19 +7,20 @@ const http = require("http");
 const socketIo = require("socket.io");
 require("dotenv").config();
 const User = require("./models/userModel");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 const CLIENT_URL = process.env.REACT_APP_API_URL;
 
-const corsOptions = {
-  origin: CLIENT_URL,
-  methods: ["GET", "POST"],
-  allowedHeaders: ["Content-Type"],
-};
-
-app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 
 mongoose
@@ -27,24 +28,15 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("DB Connection Successful");
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  .then(() => console.log("DB Connection Successful"))
+  .catch((err) => console.error(err.message));
 
-app.get("/ping", (_req, res) => {
-  return res.json({ msg: "Ping Successful" });
-});
-
+app.get("/ping", (_req, res) => res.json({ msg: "Ping Successful" }));
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Create HTTP server
+// Create HTTP server and initialize Socket.IO
 const server = http.createServer(app);
-
-// Initialize Socket.IO
 const io = socketIo(server, {
   cors: {
     origin: CLIENT_URL,
@@ -55,8 +47,11 @@ const io = socketIo(server, {
 
 global.onlineUsers = new Map();
 io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
   global.chatSocket = socket;
+
   socket.on("add-user", (userId) => {
+    console.log(`User ${userId} connected`);
     onlineUsers.set(userId, socket.id);
   });
 
@@ -74,23 +69,25 @@ io.on("connection", (socket) => {
 
       const sendUserSocket = onlineUsers.get(data.to);
       if (sendUserSocket) {
+        console.log("Sending message to:", sendUserSocket);
         socket.to(sendUserSocket).emit("msg-recieve", data.msg);
       }
     } catch (error) {
       console.error("Error handling message:", error);
     }
   });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
 
 // Serve static files from the client build directory
-const path = require("path");
 app.use(express.static(path.join(__dirname, "../client/build")));
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"))
+);
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
-
-// Start the server
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
